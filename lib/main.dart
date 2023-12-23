313c81ba635cee090e1882df07375471a00b988f
dingPie <cpie1216@gmail.com>
2023-12-23 09:41:39 +0900

    env 환경설정

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_init/providers/user.dart';
 import 'package:flutter_init/screens/router.dart';
 import 'package:flutter_init/theme/app_theme.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future main() async {
+  await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 }
 
