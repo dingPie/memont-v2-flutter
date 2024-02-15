8ddf28807f1e083afa6ee3c5968a886d91fbf92f
dingPie <cpie1216@gmail.com>
2024-02-16 07:23:15 +0900

    app state 로직 변경, provider noti 추가

--- lib/global_state/provider/app_state.dart
+++ lib/global_state/provider/app_state.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 class AppState extends ChangeNotifier {
-  String? userName;
   // P_TODO: 현재 userName이랑 상태를 공유하긴 하나, 정확한 목적 분리 떄문에 일단 상태도 분리함
-  bool isLoading = false;
-  bool isLogin = false;
+  bool _isLoading = false;
+  bool _isLogin = false;
+
+  bool get isLoading => _isLoading;
+  bool get isLogin => _isLogin;
+
+  set isLoading(bool value) {
+    _isLoading = value;
+    notifyListeners();
+  }
+
+  set isLogin(bool value) {
+    _isLogin = value;
+    notifyListeners();
+  }
 
   AppState({
-    required this.isLogin,
-  });
+    required bool isLogin,
+  }) : _isLogin = isLogin;
 }
