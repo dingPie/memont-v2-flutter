5c9e0e574a02e1d66f703223abed2afc3aecbbe8
dingPie <cpie1216@gmail.com>
2024-03-16 18:23:50 +0900

    tag screen app bar 구현

--- lib/screens/tag_screen/tag_screen.dart
+++ lib/screens/tag_screen/tag_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import 'package:memont_v2/config/build_context_extension.dart';
 import 'package:memont_v2/constants/routes.dart';
+import 'package:memont_v2/screens/login_screen/widgets/common_app_bar/app_bar_icon_button.dart';
+import 'package:memont_v2/screens/login_screen/widgets/common_app_bar/common_app_bar.dart';
+import 'package:memont_v2/widgets/common_layout.dart';
 
 class TagScreen extends StatelessWidget {
   const TagScreen({
@@ -10,17 +14,34 @@ class TagScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              ROUTES.tag.name,
-              style: context.textStyle.h1
-                  .copyWith(fontSize: 40, color: context.colors.primary[500]),
-            ),
-          ],
+    return CommonLayout(
+      child: Scaffold(
+        appBar: const PreferredSize(
+          preferredSize: Size.fromHeight(60.0),
+          child: CommonAppBar(
+            iconButtonList: [
+              AppBarIconButton(
+                routes: ROUTES.setting,
+                iconData: FontAwesomeIcons.gear,
+              ),
+              AppBarIconButton(
+                routes: ROUTES.talk,
+                iconData: FontAwesomeIcons.solidMessage,
+              )
+            ],
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                ROUTES.tag.name,
+                style: context.textStyle.h1
+                    .copyWith(fontSize: 40, color: context.colors.primary[500]),
+              ),
+            ],
+          ),
         ),
       ),
     );
