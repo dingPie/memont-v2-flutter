3633336a2514feb313fc395f0173c913943dbe23
dingPie <cpie1216@gmail.com>
2024-04-12 20:38:41 +0900

    palette class 변경

--- lib/global_state/singleton_storage.dart
+++ lib/global_state/singleton_storage.dart
@@ -1,11 +1,11 @@
 // P_MEMO: 상수값을 저장하기 위한 SingletonStorage. widget단에서 쓰지 않기 떄문에 따로 관리중임.
 // 해당 구조는 변경될 수 있음
 // /
-import 'package:memont_v2/models/palette.dart';
+import 'package:memont_v2/models/palette_dto.dart';
 
 final class SingletonStorage {
   String? accessToken;
-  List<Palette>? paletteList;
+  List<PaletteDto>? paletteList;
 
   SingletonStorage._();
   // private 생성자로 정의한 생성자를 static 영역에 할당해버린다.
